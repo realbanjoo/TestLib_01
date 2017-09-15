17a4968b795d52b68d91be1934e12f03d380e79b
Sébastien Rouet <s.rouet@MBP-JUMP1.local>
2017-09-15 10:01:11 +0200

    FantasticView in TestLib_01

---
+++ TestLib_01/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TestLib_01
+//
+//  Created by Sébastien Rouet on 15/09/2017.
+//  Copyright © 2017 Sébastien Rouet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myDouble: Double?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myDouble = 10
+        let fantasticview = FantasticView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticview)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
