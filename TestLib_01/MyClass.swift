ad36260103dcb27b5b2e8244dd2cd4061df4ef95
Sébastien Rouet <s.rouet@MBP-JUMP1.local>
2017-09-15 11:49:39 +0200

    FIX Access Control

---
+++ TestLib_01/MyClass.swift
@@ -0,0 +1,15 @@
+//
+//  MyClass.swift
+//  TestLib_01
+//
+//  Created by Sébastien Rouet on 15/09/2017.
+//  Copyright © 2017 Sébastien Rouet. All rights reserved.
+//
+
+import Foundation
+
+open class  MyClass  {
+    
+    public var MyDouble : Double = 10
+    
+}
--- TestLib_01/ViewController.swift
+++ TestLib_01/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    public static var myDouble: Double = 10
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
